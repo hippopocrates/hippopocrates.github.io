acfec8effbc85041d47da623ce5b96ba9592c37f
BuildTools <unconfigured@null.spigotmc.org>
2019-01-11 00:01:24 -0800

    added functionality and styling

--- app.js
+++ app.js
@@ -1,81 +1,73 @@
 $(() => {
 
-const playerOrder = [];
-const cardOrder = ['ACE', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'JACK', 'QUEEN', 'KING']; //value
-let currentPosition = 0;
-const discardPile = [];
+let userTurn = true;
+const cardOrder = ['ACE', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'JACK', 'QUEEN', 'KING'] //value
+let currentPosition = 0
+const discardPile = []
 
-class User {
-  constructor() {
-    this.deckSize = 18;
-    this.deck = [];
-    this.isLying = false;
-    this.turn = false;
+class Player {
+  constructor(name, size) {
+    this.name = name;
+    this.deckSize = size
+    this.deck = []
+    this.images = []
+    this.isLying = false
   }
 }
 
-class Opponent {
-  constructor(){
-    this.deckSize = 17;
-    this.deck = [];
-    this.isLying = false;
-    this.turn = false;
-
-  }
-}
-const user = new User();
-const opponent1 = new Opponent();
-const opponent2 = new Opponent();
+const user = new Player('You', 18)
+const opponent1 = new Player('Opponent 1', 17)
+const opponent2 = new Player('Opponent 2',17)
+const playerOrder = [user, opponent1, opponent2]
 
 $.ajax({
   url: 'https://deckofcardsapi.com/api/deck/new/draw/?count=52',
   success: (data) => {
-    console.log(data);
-    buildPlayerDeck(data);
-    gamePlay(playerOrder);
+    buildPlayerDeck(data)
+    gameStart()
+    oneRound(playerOrder)
   },
   error: () => {
-    console.log('bad request');
+    console.log('bad request')
   }
 })
 
-const buildPlayerDeck = (data) => {
+const buildPlayerDeck = data => {
   for (let i = 0; i < 18; i++){
-    user.deck.push(data.cards[i]);
-    if (data.cards[i].code === 'AS') {
-      playerOrder.push(user, opponent1, opponent2);
-    }
+    user.deck.push(data.cards[i])
+    $('#cardDiv').append($('<img>').attr('src', data.cards[i].image))
   }
   for (let i = 18; i < 35; i++){
-    opponent1.deck.push(data.cards[i]);
-    if (data.cards[i].code === 'AS') {
-      playerOrder.push(opponent1, opponent2, user);
-    }
+    opponent1.deck.push(data.cards[i])
+    $('#opponent1').append($('<img>').attr('src', 'playing-card-back.jpg').addClass('cardBackEnemy1'))
   }
   for (let i = 35; i < 52; i++){
-     opponent2.deck.push(data.cards[i]);
-     if (data.cards[i].code === 'AS') {
-       playerOrder.push(opponent2, user, opponent1);
-     }
+     opponent2.deck.push(data.cards[i])
+     $('#opponent2').append($('<img>').attr('src', 'playing-card-back.jpg').addClass('cardBackEnemy2'))
+
   }
 }
 
-const cardOrderPosition = () => {
-    return cardOrder[currentPosition++];
-}
+const cardOrderPosition = () => cardOrder[currentPosition++]
 
 const discard = (player, card) => {
   for (let i = 0; i < player.deckSize; i++){
     if (player.deck[i].value === card){
-      
+      discardPile.push(player.deck.splice(i, 1))
+      player.deckSize--
+      if (player.name === 'Opponent 1'){
+        $('.cardBackEnemy1').first().remove()
+      } else if (player.name === 'Opponent 2'){
+        $('.cardBackEnemy2').first().remove()
+      }
     }
   }
 }
 
-const gamePlay = (playerOrderArr) => {
-  console.log(playerOrderArr);
+
+const oneRound = playerOrderArr => {
   for (let i = 0; i < playerOrderArr.length; i++){
-    discard(playerOrderArr[i], cardOrderPosition());
+      discard(playerOrderArr[i], cardOrderPosition())
   }
 }
 
